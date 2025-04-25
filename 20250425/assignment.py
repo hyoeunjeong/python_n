928a1775e097d2e21f6c2136d700feeec6f635fa
hyoeun.jeong <jhe0316@gmail.com>
2025-04-25 17:44:17 +0900

    과제 01

---
+++ 20250425/assignment.py
@@ -0,0 +1,26 @@
+# 2025.04.25
+#1
+print("Hello")
+
+#2
+print("Hello world")
+
+#3
+print("Hello")
+print("World")
+
+#4
+print("'Hello'")
+
+#5
+print('"Hello World"')
+
+#6
+print('"!@#$%^&*()'"'")
+
+#7
+
+#8
+print("Hello\nWorld")
+
+print("Hello\nWorld")
